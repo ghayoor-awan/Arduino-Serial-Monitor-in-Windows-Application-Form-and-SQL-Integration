1ac0a07e0aed14922dd9d6ad823b04662b14f1cb
hp <hp@DESKTOP-76NRQUB>
2023-07-03 14:48:28 +0500

    Add project files.

---
+++ Ports.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Ports
+{
+    partial class Ports
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.connectBT = new System.Windows.Forms.Button();
+            this.disconnectBT = new System.Windows.Forms.Button();
+            this.clearBT = new System.Windows.Forms.Button();
+            this.scanBT = new System.Windows.Forms.Button();
+            this.sendBT = new System.Windows.Forms.Button();
+            this.comportCB = new System.Windows.Forms.ComboBox();
+            this.baudrateCB = new System.Windows.Forms.ComboBox();
+            this.incomingTB = new System.Windows.Forms.RichTextBox();
+            this.outgoingTB = new System.Windows.Forms.RichTextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // connectBT
+            // 
+            this.connectBT.Location = new System.Drawing.Point(22, 198);
+            this.connectBT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.connectBT.Name = "connectBT";
+            this.connectBT.Size = new System.Drawing.Size(86, 53);
+            this.connectBT.TabIndex = 0;
+            this.connectBT.Text = "CONNECT";
+            this.connectBT.UseVisualStyleBackColor = true;
+            this.connectBT.Click += new System.EventHandler(this.connectBT_Click);
+            // 
+            // disconnectBT
+            // 
+            this.disconnectBT.Enabled = false;
+            this.disconnectBT.Location = new System.Drawing.Point(22, 256);
+            this.disconnectBT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.disconnectBT.Name = "disconnectBT";
+            this.disconnectBT.Size = new System.Drawing.Size(86, 53);
+            this.disconnectBT.TabIndex = 1;
+            this.disconnectBT.Text = "DISCONNECT";
+            this.disconnectBT.UseVisualStyleBackColor = true;
+            this.disconnectBT.Click += new System.EventHandler(this.disconnectBT_Click);
+            // 
+            // clearBT
+            // 
+            this.clearBT.Location = new System.Drawing.Point(258, 198);
+            this.clearBT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.clearBT.Name = "clearBT";
+            this.clearBT.Size = new System.Drawing.Size(86, 53);
+            this.clearBT.TabIndex = 2;
+            this.clearBT.Text = "CLEAR";
+            this.clearBT.UseVisualStyleBackColor = true;
+            this.clearBT.Click += new System.EventHandler(this.clearBT_Click);
+            // 
+            // scanBT
+            // 
+            this.scanBT.Location = new System.Drawing.Point(140, 228);
+            this.scanBT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.scanBT.Name = "scanBT";
+            this.scanBT.Size = new System.Drawing.Size(86, 53);
+            this.scanBT.TabIndex = 3;
+            this.scanBT.Text = "SCAN";
+            this.scanBT.UseVisualStyleBackColor = true;
+            this.scanBT.Click += new System.EventHandler(this.scanBT_Click);
+            // 
+            // sendBT
+            // 
+            this.sendBT.Enabled = false;
+            this.sendBT.Location = new System.Drawing.Point(258, 256);
+            this.sendBT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.sendBT.Name = "sendBT";
+            this.sendBT.Size = new System.Drawing.Size(86, 53);
+            this.sendBT.TabIndex = 4;
+            this.sendBT.Text = "SEND";
+            this.sendBT.UseVisualStyleBackColor = true;
+            this.sendBT.Click += new System.EventHandler(this.sendBT_Click);
+            // 
+            // comportCB
+            // 
+            this.comportCB.FormattingEnabled = true;
+            this.comportCB.Location = new System.Drawing.Point(22, 156);
+            this.comportCB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comportCB.Name = "comportCB";
+            this.comportCB.Size = new System.Drawing.Size(146, 21);
+            this.comportCB.TabIndex = 5;
+            // 
+            // baudrateCB
+            // 
+            this.baudrateCB.FormattingEnabled = true;
+            this.baudrateCB.Items.AddRange(new object[] {
+            "9600",
+            "115200"});
+            this.baudrateCB.Location = new System.Drawing.Point(198, 156);
+            this.baudrateCB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.baudrateCB.Name = "baudrateCB";
+            this.baudrateCB.Size = new System.Drawing.Size(146, 21);
+            this.baudrateCB.TabIndex = 6;
+            // 
+            // incomingTB
+            // 
+            this.incomingTB.Location = new System.Drawing.Point(22, 19);
+            this.incomingTB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.incomingTB.Name = "incomingTB";
+            this.incomingTB.Size = new System.Drawing.Size(150, 126);
+            this.incomingTB.TabIndex = 7;
+            this.incomingTB.Text = "";
+            // 
+            // outgoingTB
+            // 
+            this.outgoingTB.Location = new System.Drawing.Point(198, 19);
+            this.outgoingTB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.outgoingTB.Name = "outgoingTB";
+            this.outgoingTB.Size = new System.Drawing.Size(150, 126);
+            this.outgoingTB.TabIndex = 8;
+            this.outgoingTB.Text = "";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // Ports
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 327);
+            this.Controls.Add(this.outgoingTB);
+            this.Controls.Add(this.incomingTB);
+            this.Controls.Add(this.baudrateCB);
+            this.Controls.Add(this.comportCB);
+            this.Controls.Add(this.sendBT);
+            this.Controls.Add(this.scanBT);
+            this.Controls.Add(this.clearBT);
+            this.Controls.Add(this.disconnectBT);
+            this.Controls.Add(this.connectBT);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Ports";
+            this.Text = "Ports";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connectBT;
+        private System.Windows.Forms.Button disconnectBT;
+        private System.Windows.Forms.Button clearBT;
+        private System.Windows.Forms.Button scanBT;
+        private System.Windows.Forms.Button sendBT;
+        private System.Windows.Forms.ComboBox comportCB;
+        private System.Windows.Forms.ComboBox baudrateCB;
+        private System.Windows.Forms.RichTextBox incomingTB;
+        private System.Windows.Forms.RichTextBox outgoingTB;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ Ports.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ports
+{
+    public partial class Ports : Form
+    {
+        public Ports()
+        {
+            InitializeComponent();
+        }
+
+        private void scanBT_Click(object sender, EventArgs e)
+        {
+            comportCB.Text = "";
+            comportCB.Items.Clear(); //clearing present in the comport
+            String[] ports = SerialPort.GetPortNames(); //initializing COM Port
+            comportCB.Items.AddRange(ports);
+        }
+
+        private void connectBT_Click(object sender, EventArgs e)
+        {
+            if(ConnectPort() == true)
+            {
+                connectBT.Enabled = false; //Will disable the connect button
+                disconnectBT.Enabled = true; //Will enable the Disconnect button
+                comportCB.Enabled = false; //Prohibiting the user from changing the com port after connection
+                baudrateCB.Enabled = false; //Prohibiting the user from changing the baud rate after connection
+                scanBT.Enabled = false;
+                sendBT.Enabled  = true;
+            }
+        }
+
+        public bool ConnectPort() //This fun will generate error box if com or baud rate box is empty
+        {
+            try {
+                if(comportCB.Text != "" || baudrateCB.Text != "") 
+                {
+                    serialPort1.PortName = comportCB.Text;
+                    serialPort1.BaudRate = Int32.Parse(baudrateCB.Text);
+                    serialPort1.Open();
+                    return true;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK);
+            }
+            return false;
+        }
+
+        private void disconnectBT_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            connectBT.Enabled = true;
+            disconnectBT.Enabled = false;
+            comportCB.Enabled = true;
+            baudrateCB.Enabled = true;
+            scanBT.Enabled = true;
+            sendBT.Enabled = false;
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            this.Invoke(new EventHandler(serialPort1_DataReceived));
+        }
+        private bool allowDataEntry = true;
+        private int rowCount = 0;
+        private const int MaxEntries = 30;
+        private void serialPort1_DataReceived(object sender, EventArgs e)
+        {
+            string dump = serialPort1.ReadLine();
+            incomingTB.Text = incomingTB.Text + dump;
+            string[] values = dump.Split(',');
+            if (values.Length >= 2)
+            {
+                string Sensor1 = values[0];
+                string Sensor2 = values[1];
+
+                using (SqlConnection connection = new SqlConnection("Data Source=DESKTOP-76NRQUB\\SQLEXPRESS;Initial Catalog=MyTestDB;Integrated Security=True;Pooling=False"))
+                {
+                    connection.Open();
+
+                    // Check if max entries have been reached
+                    string countQuery = "SELECT COUNT(*) FROM Arduino";
+                    using (SqlCommand countCommand = new SqlCommand(countQuery, connection))
+                    {
+                        int currentEntryCount = (int)countCommand.ExecuteScalar();
+
+                        if (currentEntryCount >= MaxEntries)
+                        {
+                            // Get the minimum RowNumber of the oldest entries
+                            string minRowNumberQuery = "SELECT MIN(RowNumber) FROM (SELECT TOP (@EntriesToDelete) RowNumber FROM Arduino ORDER BY RowNumber ASC) AS T";
+                            using (SqlCommand minRowNumberCommand = new SqlCommand(minRowNumberQuery, connection))
+                            {
+                                minRowNumberCommand.Parameters.AddWithValue("@EntriesToDelete", currentEntryCount - MaxEntries + 1);
+                                int minRowNumber = (int)minRowNumberCommand.ExecuteScalar();
+
+                                // Delete the oldest entries
+                                string deleteQuery = "DELETE FROM Arduino WHERE RowNumber <= @MinRowNumber";
+                                using (SqlCommand deleteCommand = new SqlCommand(deleteQuery, connection))
+                                {
+                                    deleteCommand.Parameters.AddWithValue("@MinRowNumber", minRowNumber);
+                                    deleteCommand.ExecuteNonQuery();
+                                }
+                            }
+
+                            // Reset rowCount to 0
+                            rowCount = 0;
+                        }
+                    }
+
+                    // Retrieve the last used RowNumber from the database
+                    string getLastRowNumberQuery = "SELECT MAX(RowNumber) FROM Arduino";
+                    using (SqlCommand getLastRowNumberCommand = new SqlCommand(getLastRowNumberQuery, connection))
+                    {
+                        object lastRowNumberObj = getLastRowNumberCommand.ExecuteScalar();
+                        int lastRowNumber = (lastRowNumberObj != DBNull.Value) ? Convert.ToInt32(lastRowNumberObj) : 0;
+                        int newRowNumber = lastRowNumber + 1;
+
+                        string sql = "INSERT INTO Arduino (RowNumber, Sensor1, Sensor2) VALUES (@RowNumber, @Sensor1, @Sensor2)";
+                        using (SqlCommand command = new SqlCommand(sql, connection))
+                        {
+                            // Add parameters to the command
+                            command.Parameters.AddWithValue("@RowNumber", newRowNumber);
+                            command.Parameters.AddWithValue("@Sensor1", Sensor1);
+                            command.Parameters.AddWithValue("@Sensor2", Sensor2);
+
+                            int rowsAffected = command.ExecuteNonQuery();
+
+                            if (rowsAffected > 0)
+                            {
+                                // Successfully inserted data, update rowCount
+                                rowCount = newRowNumber;
+                            }
+                            else
+                            {
+                                MessageBox.Show("Failed to insert data into the database.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        private void clearBT_Click(object sender, EventArgs e)
+        {
+            incomingTB.Text = "";
+        }
+
+        private void sendBT_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!(serialPort1.IsOpen)) { serialPort1.Open(); }
+                serialPort1.Write(outgoingTB.Text);
+                outgoingTB.Text = "";
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
